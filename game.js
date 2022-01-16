2f2a24c2764d43faa02985b12f037c502cfa16ba
Nimrod Allan Musungu <80291864+alroude@users.noreply.github.com>
2022-01-16 19:00:55 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,193 +1,173 @@
-'use strict'
+"use strict";
 
 // When a button is clicked, display the intended game
+//   document.getElementById("guess").style.display = "none";
+//   document.getElementById("calculator").style.display = "none";
+//   document.getElementById("abc").style.display = "none";
+//   document.getElementById("tipCalc").style.display = "none";
+//   document.getElementById("todo").style.display = "none";
 
+//   const displayTab = function(btn){
+//       document.getElementById(btn).onclick = function () {
+//           if(btn1){
+//             document.getElementById("guess").style.display = "block";
+//           }else if(btn2){
+//             document.getElementById("abc").style.display = "block";
+//           }else if(btn3){
+//             document.getElementById("calculator").style.display = "block";
+//           }else if(btn4){
+//             document.getElementById("tipCalc").style.display = "block";
+//           }else{
+//             document.getElementById("todo").style.display = "block";
+//           }
+//       }
+//   }
 
 // When Button 1 is clicked
-document.getElementById('btn1').onclick = function() {
-    document.getElementById('guess').style.display = "block";
-    document.getElementById('calculator').style.display = "none";
-    document.getElementById('abc').style.display = "none";
-    document.getElementById('tipCalc').style.display = "none";
-    document.getElementById('todo').style.display = "none";
-}
+
+document.getElementById("btn1").onclick = function () {
+  document.getElementById("guess").style.display = "block";
+  document.getElementById("calculator").style.display = "none";
+  document.getElementById("tipCalc").style.display = "none";
+  document.getElementById("todo").style.display = "none";
+  document.getElementById("cart").style.display = "none";
+};
 
 // When Button 2 is clicked
-document.getElementById('btn2').onclick = function() {
-        document.getElementById('abc').style.display = "block";
-        document.getElementById('guess').style.display = "none";
-        document.getElementById('calculator').style.display = "none";
-        document.getElementById('tipCalc').style.display = "none";
-        document.getElementById('todo').style.display = "none";
-}
+// document.getElementById("btn2").onclick = function () {
+//   document.getElementById("abc").style.display = "block";
+//   document.getElementById("guess").style.display = "none";
+//   document.getElementById("calculator").style.display = "none";
+//   document.getElementById("tipCalc").style.display = "none";
+//   document.getElementById("todo").style.display = "none";
+//   document.getElementById("cart").style.display = "none";
+// };
 
 // When Button 3 is clicked
-document.getElementById('btn3').onclick = function() {
-    document.getElementById('calculator').style.display = "block";
-    document.getElementById('abc').style.display = "none";
-    document.getElementById('guess').style.display = "none";
-    document.getElementById('tipCalc').style.display = "none";
-    document.getElementById('todo').style.display = "none";
-}
+document.getElementById("btn3").onclick = function () {
+  document.getElementById("calculator").style.display = "block";
+  document.getElementById("guess").style.display = "none";
+  document.getElementById("tipCalc").style.display = "none";
+  document.getElementById("todo").style.display = "none";
+  document.getElementById("cart").style.display = "none";
+};
 
 // When Button 4 is clicked
-document.getElementById('btn4').onclick = function() {
-    document.getElementById('tipCalc').style.display = "block";
-    document.getElementById('abc').style.display = "none";
-    document.getElementById('guess').style.display = "none";
-    document.getElementById('calculator').style.display = "none";
-    document.getElementById('todo').style.display = "none";
-}
+document.getElementById("btn4").onclick = function () {
+  document.getElementById("tipCalc").style.display = "block";
+  document.getElementById("guess").style.display = "none";
+  document.getElementById("calculator").style.display = "none";
+  document.getElementById("todo").style.display = "none";
+  document.getElementById("cart").style.display = "none";
+};
 
 // When Button 5 is clicked
-document.getElementById('btn5').onclick = function() {
-    document.getElementById('todo').style.display = "block";
-    document.getElementById('abc').style.display = "none";
-    document.getElementById('guess').style.display = "none";
-    document.getElementById('calculator').style.display = "none";
-    document.getElementById('tipCalc').style.display = "none";
-}
+document.getElementById("btn5").onclick = function () {
+  document.getElementById("todo").style.display = "block";
+  document.getElementById("guess").style.display = "none";
+  document.getElementById("calculator").style.display = "none";
+  document.getElementById("tipCalc").style.display = "none";
+  document.getElementById("cart").style.display = "none";
+};
 
-// GUESS MY AGE GAME 
-const myAge = 50;
-document.getElementById("submit").onclick = function() {
-    let age = parseInt(document.getElementById("age").value);
-    if(age < myAge) {
-        document.getElementById("error-msg").style.display = "block";
-        document.getElementById("error-msg").innerHTML  = `Eeehh!😒 The number is higher than ${age} Try Again!`;
-    }else if(age > myAge) {
-        document.getElementById("error-msg").style.display = "block";
-        document.getElementById("error-msg").innerHTML = `Oops!😜 The number is lower than ${age}. Try Again!`;
-    }else if(age == myAge) {
-        document.getElementById("results-box").style.display = "block";
-        document.getElementById("results").innerHTML = `You Got it! 😎 The number is ${age}`;
-    }else if(age !== "") {
-        alert("You are kidding, right 😁. Please enter a number");
+// When Button 6 is clicked
+document.getElementById("btn6").onclick = function () {
+  document.getElementById("cart").style.display = "block";
+  document.getElementById("guess").style.display = "none";
+  document.getElementById("calculator").style.display = "none";
+  document.getElementById("tipCalc").style.display = "none";
+  document.getElementById("todo").style.display = "none";
+};
+
+// GUESS MY AGE GAME
+let myAge = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+document.getElementById("submit").onclick = function () {
+  let age = Number(document.querySelector("#age").value);
+  Number(document.querySelector("#age").value = "");
+
+  if(age === ''){
+    document.querySelector(".error-msg").textContent = "⛔Please Enter a Number";
+  }
+  
+// When number is equal to guessed number
+  else if (age === myAge) {
+    document.getElementById("results").style.display = "block";
+    document.getElementById("results").innerHTML = `You Got it! 😎`;
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".correct").textContent = age;
+    document.querySelector(".correct").style.background = "var(--color4)";
+    document.querySelector(".correct").style.color = "#ffffff";
+    document.querySelector(".hr-number").style.background = "var(--color4)";
+    document.querySelector(".correct").style.border = "none";
+    if(score > highscore){
+        highscore = score;
+    document.querySelector(".highscore").textContent = score;
     }
-    else {
-        document.getElementById("error-msg").innerHTML = `You are kidding, right 😁`;
+  } 
+//   When number is NOT equal to guessed number
+  else if (age !== myAge) {
+    document.querySelector("#error-msg").style.display = "block";
+    document.querySelector("#error-msg").textContent =
+      age < myAge
+        ? `Eeehh!😒 The number is higher than ${age} Try Again!`
+        : `Oops!😜 The number is lower than ${age}. Try Again!`;
+    if (score > 0) {
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".error-msg").textContent = "You Lose 😞";
     }
-}
+  }
+
+
+//   RESET BUTTON TO PLAY AGAIN
+document.querySelector('#reset').addEventListener('click', function(){
+    score = 20;
+    document.querySelector(".score").textContent = score;
+    document.getElementById("results").style.display = "none";
+    document.querySelector(".correct").textContent = '?';
+    document.querySelector(".correct").style.background = "#fff";
+    document.querySelector(".correct").style.color = "#000";
+    document.querySelector(".hr-number").style.background = "var(--color2)";
+    document.querySelector(".correct").style.border = "5px solid var(--color2)";
+    Number(document.querySelector("#age").value = "");
+    document.querySelector(".error-msg").style.display = "none";
+    myAge = Math.trunc(Math.random() * 20 + 1);
+
+});
+
+
+// //   If NO number is entered
+//   else if(age == ''){
+//     document.querySelector(".error-msg").textContent = "⛔Please Enter a Number";
+//   }else{
+//     document.querySelector(".error-msg").textContent = "⛔Please Enter a Number";
+//   }
+};
+//     if(age < myAge) {
+//         if(score > 1){
+//             document.querySelector("#error-msg").style.display = "block";
+//             document.querySelector("#error-msg").textContent  = `Eeehh!😒 The number is higher than ${age} Try Again!`;
+//             score--;
+//             document.querySelector('.score').textContent = score;
+//         }
+
+//     }else if(age > myAge) {
+//         if(score > 0){
+//             document.getElementById("error-msg").style.display = "block";
+//             document.getElementById("error-msg").innerHTML = `Oops!😜 The number is lower than ${age}. Try Again!`;
+//             score--;
+//             document.querySelector('.score').textContent = score;
+//         }else {
+//             document.querySelector('.error-msg').textContent = "You Lose 😞";
+//         }
+
+//     }
 
 // BALLOONS GAME
 
-document.getElementById('a').onclick = function() {
-    document.getElementById('a').style.display = "none";
-    // alert(`🍎 A for Apple`);
-}
-
-document.getElementById('b').onclick = function() {
-    document.getElementById('b').style.display = "none";
-    // alert(`B for Ball ⚽`);
-}
-
-document.getElementById('c').onclick = function() {
-    document.getElementById('c').style.display = "none";
-    // alert(`C for Cat 🐈`);
-}
-
-document.getElementById('d').onclick = function() {
-    document.getElementById('d').style.display = "none";
-    // alert(`D for Dog 🐕`);
-}
-
-document.getElementById('e').onclick = function() {
-    document.getElementById('e').style.display = "none";
-    // alert(`E for Egg 🥚`);
-}
-
-document.getElementById('f').onclick = function() {
-    document.getElementById('f').style.display = "none";
-    // alert(`F for Fish 🐟`);
-}
-
-document.getElementById('g').onclick = function() {
-    document.getElementById('g').style.display = "none";
-    // alert(`G for Giraffe 🦒`);
-}
-
-document.getElementById('h').onclick = function() {
-    document.getElementById('h').style.display = "none";
-    // alert(`H for House 🏠`);
-}
-
-document.getElementById('i').onclick = function() {
-    document.getElementById('i').style.display = "none";
-    // alert(`I for Inkpot`);
-}
-
-document.getElementById('j').onclick = function() {
-    document.getElementById('j').style.display = "none";
-    // alert(`J for Jug`);
-}
-
-document.getElementById('k').onclick = function() {
-    document.getElementById('k').style.display = "none";
-    // alert(`<h2>K for Kite 🪁</h2>`);
-}
-
-document.getElementById('l').onclick = function() {
-    document.getElementById('l').style.display = "none";
-    // alert(`L for Lion 🦁`);
-}
-
-document.getElementById('m').onclick = function() {
-    document.getElementById('m').style.display = "none";
-    // alert(`M for Mango 🥭`);
-}
-
-document.getElementById('n').onclick = function() {
-    document.getElementById('n').style.display = "none";
-    // alert(`N for Net 🥅`);
-}
-
-document.getElementById('o').onclick = function() {
-    document.getElementById('o').style.display = "none";
-}
-
-document.getElementById('p').onclick = function() {
-    document.getElementById('p').style.display = "none";
-}
-
-document.getElementById('q').onclick = function() {
-    document.getElementById('q').style.display = "none";
-}
-
-document.getElementById('r').onclick = function() {
-    document.getElementById('r').style.display = "none";
-}
-
-document.getElementById('s').onclick = function() {
-    document.getElementById('s').style.display = "none";
-}
-
-document.getElementById('t').onclick = function() {
-    document.getElementById('t').style.display = "none";
-}
-
-document.getElementById('u').onclick = function() {
-    document.getElementById('u').style.display = "none";
-}
-
-document.getElementById('v').onclick = function() {
-    document.getElementById('v').style.display = "none";
-}
-
-document.getElementById('w').onclick = function() {
-    document.getElementById('w').style.display = "none";
-}
-
-document.getElementById('x').onclick = function() {
-    document.getElementById('x').style.display = "none";
-}
-
-document.getElementById('y').onclick = function() {
-    document.getElementById('y').style.display = "none";
-}
-
-document.getElementById('z').onclick = function() {
-    document.getElementById('z').style.display = "none";
-}
 
 // // Count Function
 // const num1 = document.getElementById('z').onclick = function() {
@@ -212,28 +192,68 @@ document.getElementById('z').onclick = function() {
 
 // BMI CALCULATOR
 
-document.getElementById("calcbmi").onclick = function() {
-    let weight = document.getElementById("weight").value;
-    const height = document.getElementById('height').value;
-    const bmi = weight / (height * height);
-   document.getElementById('bmiResults').innerHTML = Math.round(bmi);
-    console.log(weight);
+// When Calculate BMI button is clicked
+document.getElementById("calcbmi").onclick = function () {
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value;
+  const bmi = weight / (height * height);
+
+  document.getElementById("bmiResults").innerHTML = bmi.toFixed(1);
+  document.getElementById('index').style.display = "block";  
+  // document.getElementById('image').style.display = "block"; 
+  let image = document.querySelectorAll('image1');
+  // When weight is underweight
+  if(bmi < 18.5){
+    document.getElementById('index').innerHTML = "Underweight = <18.5";
+    document.getElementById('index').style.backgroundColor = "#06D4B5";
+    document.getElementById('index').style.color = "white";
+    document.getElementById('image1').style.display = "block";
+  }
+  // When weight is normal
+  else if(bmi >= 18.5 && bmi <= 24.9){
+    document.getElementById('index').innerHTML = "Normal weight = 18.5–24.9";
+    document.getElementById('index').style.backgroundColor = "var(--color4)";
+    document.getElementById('index').style.color = "white";
+    document.getElementById('image2').style.display = "block";
+  }
+  // When weight is overweight
+    else if(bmi >= 25 && bmi <= 29.9){
+      document.getElementById('index').innerHTML = "Overweight = 25–29.9";
+      document.getElementById('index').style.backgroundColor = "var(--color2)";
+      document.getElementById('index').style.color = "white";
+      document.getElementById('image3').style.display = "block";
+    }
+    // When weight is obesity
+    else if(bmi >= 30){
+      document.getElementById('index').innerHTML = "Obese = BMI of 30 or greater";
+      document.getElementById('index').style.backgroundColor = "red";
+      document.getElementById('index').style.color = "white";
+      document.getElementById('image4').style.display = "block";
+    }
+};
+
+// When Reset button is clicked
+document.getElementById('resetbmi').onclick = function(){
+  weight = document.getElementById("weight").innerHTML = "";
+  height = document.getElementById("height").innerHTML = "";
+  document.getElementById('image').style.display = "none"; 
+  document.getElementById('index').style.display = "none";
 }
 
 //TIP CALCULATOR
 
-
-document.getElementById("calcTip").onclick = function() {
-    function calcTip(bill) {
-        return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-    }
-    const bill = document.getElementById("bill").value;
-    const tip = calcTip(bill);
-    const finalValue = bill + tip;
-    bill === "" ? alert("Please Enter a bill amount first to calculate tip!") : 
-    document.getElementById("billDisplay").innerHTML = "$" + bill;
-    document.getElementById("tipDisplay").innerHTML = "$" + tip;
-    document.getElementById("totalDisplay").innerHTML = "$" + finalValue;
+document.getElementById("calcTip").onclick = function () {
+  function calcTip(bill) {
+    return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+  }
+  const bill = document.getElementById("bill").value;
+  const tip = calcTip(bill);
+  const finalValue = Number(bill) + Number(tip);
+  bill === ""
+    ? alert("Please Enter a bill amount first to calculate tip!")
+    : (document.getElementById("billDisplay").innerHTML = "$" + bill);
+  document.getElementById("tipDisplay").innerHTML = "$" + tip;
+  document.getElementById("totalDisplay").innerHTML = "$" + finalValue;
 };
 
 // TODO LIST APP
@@ -253,45 +273,58 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("todoList").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("todos").appendChild(li);
-    }
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("todoList").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todos").appendChild(li);
+  }
 }
 document.getElementById("todoList").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+// var span = document.createElement("SPAN");
+// var txt = document.createTextNode("\u00D7");
+// span.className = "close";
+// span.appendChild(txt);
+// li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
 
+// SHOPPING CART
+// const itemPrice1  = 750;
+// const itemPrice2 = 550;
+// const itemPrice3 = 650;
+// let viewCart = 0;
 
+document.getElementById("viewcart").onclick = function () {
+  document.getElementById('showcart').style.display = "block";
+  document.getElementById('cartCount').innerHTML = "X";
+};
